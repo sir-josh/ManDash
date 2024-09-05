ca4db00f372cf5ea8d95e765197ebba95859e83d
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2024-09-05 17:34:20 +0100

    Add: create admin dashboard ui schematics

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,23 @@
+import UserCard from "@/components/UserCard";
 
 function AdminPage() {
-  return (
-    <div>Page</div>
-  )
+	return (
+		<div className="p-4 flex gap-4 flex-col md:flex-row ">
+			{/* LEFT */}
+			<div className="w-full lg:w-2/3">
+				{/* USER CARD */}
+				<div className="flex flex-wrap gap-4 justify-between">
+					<UserCard type="student" />
+					<UserCard type="teacher" />
+					<UserCard type="parent" />
+					<UserCard type="staff" />
+				</div>
+			</div>
+
+			{/* RIGHT */}
+			<div className="w-full lg:w-1/3">right side</div>
+		</div>
+	);
 }
 
-export default AdminPage
+export default AdminPage;
