b8d7e402bfe10527056f52f1416d1c98a6e39b92
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2024-09-05 23:47:18 +0100

    Add: create Attendance barchart on Admin page

---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,106 @@
+"use client";
+import Image from "next/image";
+
+import {
+	BarChart,
+	Bar,
+	Rectangle,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+} from "recharts";
+
+const data = [
+	{
+		name: "Mon",
+		present: 60,
+		absent: 40,
+	},
+	{
+		name: "Tue",
+		present: 70,
+		absent: 60,
+	},
+	{
+		name: "Wed",
+		present: 90,
+		absent: 75,
+	},
+	{
+		name: "Thu",
+		present: 90,
+		absent: 75,
+	},
+	{
+		name: "Fri",
+		present: 65,
+		absent: 55,
+	},
+];
+
+function AttendanceChart() {
+	return (
+		<div className="bg-white rounded-xl w-full h-full p-4">
+			{/* TITLE */}
+			<div className="flex justify-between items-center ">
+				<h1 className="text-lg font-semibold">Attendance</h1>
+				<Image src="/moreDark.png" alt="" width={20} height={20} />
+			</div>
+
+			{/* CHART */}
+			<div className="relative w-full h-[90%]">
+				<ResponsiveContainer>
+					<BarChart width={500} height={300} data={data} barSize={20}>
+						<CartesianGrid
+							strokeDasharray="3 3"
+							vertical={false}
+							stroke="#ddd"
+						/>
+						<XAxis
+							dataKey="name"
+							axisLine={false}
+							tickLine={false}
+							tick={{ fill: "#d1d5db" }}
+						/>
+						<YAxis
+							axisLine={false}
+							tickLine={false}
+							tick={{ fill: "#d1d5db" }}
+						/>
+						<Tooltip
+							contentStyle={{
+								borderRadius: "10px",
+								borderColor: "lightgray",
+							}}
+						/>
+						<Legend
+							align="left"
+							verticalAlign="top"
+							wrapperStyle={{
+								paddingTop: "20px",
+								paddingBottom: "40px",
+							}}
+						/>
+						<Bar
+							dataKey="present"
+							fill="#FAE27C"
+							legendType="circle"
+							radius={[10, 10, 0, 0]}
+						/>
+						<Bar
+							dataKey="absent"
+							fill="#C3EBFA"
+							legendType="circle"
+							radius={[10, 10, 0, 0]}
+						/>
+					</BarChart>
+				</ResponsiveContainer>
+			</div>
+		</div>
+	);
+}
+
+export default AttendanceChart;
