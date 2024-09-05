030974426b5c3dfcde32b2beef92715dcd26fef1
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2024-09-06 00:26:11 +0100

    Add: create finance chart on admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 function AdminPage() {
@@ -23,12 +24,14 @@ function AdminPage() {
 					</div>
 					{/* ATTENDANCE CHART */}
 					<div className="w-full lg:w-2/3 h-[450px]">
-            <AttendanceChart />
-          </div>
+						<AttendanceChart />
+					</div>
 				</div>
 
 				{/* BOTTOM CHART */}
-				<div className=""></div>
+				<div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
 			</div>
 
 			{/* RIGHT */}
