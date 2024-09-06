5e599590564bd4d7125b4c1584bf058e565b538f
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2024-09-06 21:50:54 +0100

    Add: create parent page widget components

--- src/app/(dashboard)/parents/page.tsx
+++ src/app/(dashboard)/parents/page.tsx
@@ -1,5 +1,23 @@
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
+
 function ParentsPage() {
-	return <div>ParentsPage</div>;
+	return (
+		<div className="p-4 flex gap-4 flex-col xl:flex-row flex-1">
+			{/* LEFT */}
+			<div className="w-full xl:w-2/3">
+				<div className="h-full bg-white p-4 rounded-md">
+					<h1 className="text-xl font-semibold">Schedule (Geraldine Brown)</h1>
+					<BigCalendar />
+				</div>
+			</div>
+
+			{/* RIGHT */}
+			<div className="w-full xl:w-1/3">
+				<Announcement />
+			</div>
+		</div>
+	);
 }
 
 export default ParentsPage;
