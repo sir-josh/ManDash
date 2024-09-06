12d735fdc2fc893550e67649141e36923d1ac181
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2024-09-06 21:46:55 +0100

    Add: create teacher page widget components

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -22,7 +22,7 @@ export default function DashboardLayout({
 			</div>
 
 			{/* RIGHT SIDE */}
-			<div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-auto">
+			<div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-auto flex flex-col">
 				<Navbar />
 				{children}
 			</div>
--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,8 +1,23 @@
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
 
 function TeacherPage() {
-    return (
-      <div>StudentPage</div>
-    )
-  }
-  
-  export default TeacherPage
+	return (
+		<div className="p-4 flex gap-4 flex-col xl:flex-row flex-1">
+			{/* LEFT */}
+			<div className="w-full xl:w-2/3">
+				<div className="h-full bg-white p-4 rounded-md">
+					<h1 className="text-xl font-semibold">Schedule </h1>
+					<BigCalendar />
+				</div>
+			</div>
+
+			{/* RIGHT */}
+			<div className="w-full xl:w-1/3">
+				<Announcement />
+			</div>
+		</div>
+	);
+}
+
+export default TeacherPage;
